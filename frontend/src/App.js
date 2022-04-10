3c0656541315ea6ee2eee63c9b2567aa0d4d1cb1
McKelvie Smith <mckelvie@MacBook-Pro.local>
2022-04-10 00:22:41 -0400

    fix login, register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ function App() {
               <Route path="home" exact={true} element={<HomeScreen />} />
               <Route path="login" element={<Login/>}/>
               <Route path="register" element={<Register/>}/>
+              {/*<Route path="profile" element={<Profile/>}/>*/}
             </Route>
           </Routes>
         </div>
--- frontend/src/components/Login/log-in.js
+++ frontend/src/components/Login/log-in.js
@@ -4,27 +4,38 @@ import {Link} from "react-router-dom";
 const Login = () => {
 
   return (
-      <div className="container mt-5">
-        {/*https://mdbootstrap.com/docs/standard/extended/login/*/}
-        <h1 className="text-center text-primary mb-5">Log in</h1>
-        <form>
-              <div className="form-outline mb-4">
-                <input type="email" id="loginName" className="form-control" />
-                <label className="form-label">Email or username</label>
-              </div>
+      <div>
+        <div className="d-inline-block mt-2">
+          <Link to="/home">
+            <button className="btn btn-primary float-end">Home</button>
+          </Link>
+        </div>
+        <div className="container mt-3">
+          {/*https://mdbootstrap.com/docs/standard/extended/login/*/}
+          <h1 className="text-center text-primary mb-5">Log in</h1>
+          <form>
+            <div className="form-outline mb-4">
+              <input type="email" id="loginName" className="form-control"
+                     placeholder="johnsmith@webdev.com"/>
+              <label className="form-label">Email or username</label>
+            </div>
 
-              <div className="form-outline mb-4">
-                <input type="password" id="loginPassword" className="form-control" />
-                <label className="form-label">Password</label>
-              </div>
+            <div className="form-outline mb-4">
+              <input type="password" id="loginPassword" className="form-control"
+                     placeholder="password"/>
+              <label className="form-label">Password</label>
+            </div>
 
-              <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
+            <button type="submit"
+                    className="btn btn-primary btn-block mb-4">Sign in
+            </button>
 
-              <div className="text-center">
-                <p>Not a member? <Link to="/register">Register</Link></p>
-              </div>
-            </form>
-          </div>
+            <div className="text-center">
+              <p>Not a member? <Link to="/register">Register</Link></p>
+            </div>
+          </form>
+        </div>
+      </div>
   )
 };
 
