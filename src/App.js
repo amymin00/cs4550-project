0f820230462fbc7f750d7419d3dc4b7c221c82df
amymin00 <amy.min7117@gmail.com>
2022-04-14 11:52:14 -0400

    pull changes from mckelvie's branch, delete backend directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './vendors/bootstrap.min.css';
+import {BrowserRouter, Routes, Route}
+  from "react-router-dom";
+import HomeScreen from "./components/HomeScreen"
+import Login from "./components/Login/log-in";
+import Register from "./components/Login/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/">
+              <Route index element={<HomeScreen />} />
+              <Route path="home" exact={true} element={<HomeScreen />} />
+              <Route path="login" element={<Login/>}/>
+              <Route path="register" element={<Register/>}/>
+              {/*<Route path="profile" element={<Profile/>}/>*/}
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
