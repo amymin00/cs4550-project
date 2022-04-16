3f4b972797e30cc2b60f09ade915b349a8011e87
McKelvie Smith <mckelvie@macbook-pro.home>
2022-04-16 14:37:39 -0400

    implement redux, create user front end fully implemented, started updating user and editing profile

--- frontend/src/components/HomeScreen/index.js
+++ frontend/src/components/HomeScreen/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../Header";
 import {Link} from "react-router-dom";
 import Footer from "../Footer";
 
 const HomeScreen = () => {
   return (
       <div>
-      <Header/>
       <div  className="align-bottom">
         <Footer />
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
