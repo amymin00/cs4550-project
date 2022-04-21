1ffad64cc2c275ce1cfab75112a061b1451a4290
Bayden Ibrahim <baydenibrahim@gmail.com>
2022-04-21 11:16:21 -0400

    fixed footer and added privacy policy

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './footer.css';
 
 const Footer = () => {
   return (
-      <div>
+      <div className="footer">
+      <div>CS4550 Final Project by Amy Min, McKelvie Smith, Shreya Mitra, and Bayden Ibrahim</div> 
         <Link to="/privacy">
           Privacy Policy
         </Link>
